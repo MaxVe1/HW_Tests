a550054f6585b047152b7caccd5b67cd5bdab5ab
maxve1 <emvi@tut.by>
2021-11-23 20:29:42 +0300

    fixed bugs in task1_test

--- specs/task1_test.js
+++ specs/task1_test.js
@@ -1,34 +1,28 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 describe('Check app', function () {
-    it('should login', async function () {
+    it('should login and check color', async function () {
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
         await $('button').click();
-        await $('#spinner').waitForExist({ reverse: false, timeout: 5000 });
-        await browser.pause(12000);      
-        const navItems = await $$('.nav-item');
-
-        const userText = await navItems[0].getText();
-        console.log(`Logged In: ${userText === "John Walker ➥"}`); //проверяет действительно ли пользователь с таким логином вошел в систему
+        await $("#spinner").waitForDisplayed({ reverse: true, timeout: 10000 });
  
-        const color = [];
-       
-        for (let i=1;i<=6;i++) { 
-            
-             const text = await navItems[i].getText();
-             console.log(text);
+        const navItems = await $$('.nav-item');
+        const userText = await navItems[0].getText();
 
-            await browser.pause(3000);          
-            navItems[i].moveTo();
-            await browser.pause(3000); 
-            color[i] = await navItems[i].getCSSProperty('background-color') 
-            console.log(color[i]);      
-            if(color[i].value === "rgba(255,0,0,1)"){
-                throw new Error(`The menu Item ${text} has wrong color!`)
-            }
+        console.log(`Logged In: ${userText === "John Walker ➥"}`); //проверяет действительно ли пользователь с таким логином вошел в систему
 
-            await browser.pause(1000);
+        for (const item of navItems){
+        const text = await item.getText();
+        console.log(text);
+        item.moveTo();
+        await browser.pause(2000);
+        const color = await item.getCSSProperty('background-color');
+        console.log(color);
+        
+        if(color.value === "rgba(255,0,0,1)"){
+            throw new Error(`The menu Item ${text} has wrong color!`);
+        }
         }
     });
 });
