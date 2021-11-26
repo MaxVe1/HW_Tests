945b4d9791df77aa69e67217e0258a7881657913
maxve1 <emvi@tut.by>
2021-11-26 15:19:41 +0300

    added checking of users

--- specs/forms1.js
+++ specs/forms1.js
@@ -1,6 +1,6 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 describe('Check app', function () {
-    it('send forms', async function () {
+    it('should login and check color', async function () {
         await browser.url('https://viktor-silakov.github.io/course-sut/index.html?quick');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
@@ -29,10 +29,39 @@ describe('Check app', function () {
         await $('#zip').setValue('50005');
         await $('#description').setValue('Added new user');
         await $('#dashboard > div > div > div > form > button').click();
-         
+
+        if ( await $("//*[@id='users-table']/div[2]/div/div[1]/div[1]").getText() !="one@mail.com" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[2]").getText() !="manager" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[3]").getText() !="Main Street" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[4]").getText() !="1234" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[6]").getText() !="US" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[7]").getText() !="10001" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[8]").getText() !="New user created" ||
+             await $("//*[@id='users-table']/div[2]/div/div[1]/div[11]").getText() !="country"
+        )     
+        {
+            throw new Error("Incorrect Values");
+        }            
+                  
+        if ( await $("//*[@id='users-table']/div[2]/div/div[2]/div[1]").getText() !="two@mail.com" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[2]").getText() !="manager" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[3]").getText() !="Green Street" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[4]").getText() !="1234" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[6]").getText() !="US" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[7]").getText() !="50005" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[8]").getText() !="Added new user" ||
+             await $("//*[@id='users-table']/div[2]/div/div[2]/div[11]").getText() !="country"
+        )        
+        {
+            throw new Error("Incorrect Values");
+        }              
         await browser.pause(2000);
     });
-});
+});   
+                
+        
+         
+
 
 
 
